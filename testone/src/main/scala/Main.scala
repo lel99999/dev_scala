4b86aecbf24936dbd7e298d8d3dde0c93277082c
lel99999 <34351721+lel99999@users.noreply.github.com>
2019-02-20 22:21:21 -0500

    Update Main.scala

--- testone/src/main/scala/Main.scala
+++ testone/src/main/scala/Main.scala
@@ -2,7 +2,9 @@ object Main extends App {
   val persons = List(
     Person(firstName = "Akira", lastName = "Sakura", age = 12),
     Person(firstName = "Peter", lastName = "Müller", age = 34),
-    Person(firstName = "Nick", lastName = "Tagart", age = 52))
+    Person(firstName = "Nick", lastName = "Tagart", age = 52),
+    Person(firstName = "Joe" , lastName = "Garfield", age = 18),
+    Person(firstName = "Eliza", lastName = "Brown", age = 25))
 
   val adults = Person.filterAdult(persons)
   val descriptions = adults.map(p => p.description).mkString("\n\t")
